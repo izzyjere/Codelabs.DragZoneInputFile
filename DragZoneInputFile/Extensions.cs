f641230b7b1ebd2b7f5ef50f302aa243daf74235
izzyjere <wisdomjere5@gmail.com>
2023-09-02 11:50:14 +0200

    Add project files.

---
+++ DragZoneInputFile/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DragZoneInputFile
+{
+    internal static class Extensions
+    {
+        internal static async ValueTask FocusDrag(this IJSObjectReference module,ElementReference elementReference, bool active = true)  
+        {
+            await module.InvokeVoidAsync("focusDrag", elementReference, active);
+        }
+    }
+}
